40a9982fb784290f80c809d834ec2ceecf6517e3
Gabriel Reis Gallaci <gabrielreisgallaci@gmail.com>
2025-10-20 15:57:36 +0000

    CARA CONECTA NO PROJETO studio-3334006320-e3114

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCpv9BYlFGfIge0XV4Rdefk_ghnEciJx2U",
   authDomain: "studio-3334006320-e3114.firebaseapp.com",
   projectId: "studio-3334006320-e3114",
-  storageBucket: "studio-3334006320-e3114.firebasestorage.app",
+  storageBucket: "studio-3334006320-e3114.appspot.com",
   messagingSenderId: "629881383785",
   appId: "1:629881383785:web:52011dc3adcfdf354e6bf8"
 };
